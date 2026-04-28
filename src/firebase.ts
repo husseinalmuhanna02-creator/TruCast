40253e3c68e72d5b9fac1a0764d066553726aeb8
husseinalmuhanna02-creator <hussein@trucast.com>
2026-04-28 07:48:06 +0300

    تصدير وظائف الدخول من ملف firebase 🚀💙

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,4 @@
+import { FirebaseAuthentication } from '@capacitor-firebase/authentication';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { initializeFirestore, memoryLocalCache } from 'firebase/firestore';
@@ -15,3 +16,11 @@ export const db = initializeFirestore(app, {
   localCache: memoryLocalCache(),
 }, databaseId);
 export const auth = getAuth(app);
+
+export const signInWithGoogle = async () => {
+  return await FirebaseAuthentication.signInWithGoogle();
+};
+
+export const signInWithFacebook = async () => {
+  return await FirebaseAuthentication.signInWithFacebook();
+};
